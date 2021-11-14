00815cfa31949f6579b8b65fe3240cda24e1e19f
wliao <lwlw1988@gmail.com>
2021-11-14 20:48:12 +0800

    add nix compat for lorri

--- scala3/flake.nix
+++ scala3/flake.nix
@@ -4,22 +4,25 @@
   nixConfig.extra-experimental-features = "nix-command flakes ca-references";
   nixConfig.substituters = "https://mirrors.ustc.edu.cn/nix-channels/store https://cache.nixos.org/";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   inputs.devshell.url = "github:numtide/devshell";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, flake-utils, devshell, nixpkgs }:
+  outputs = { self, flake-utils, devshell, nixpkgs, ... }:
     flake-utils.lib.eachDefaultSystem (system: {
-      devShell =
-        let pkgs = import nixpkgs {
+      devShell = let
+        pkgs = import nixpkgs {
           inherit system;
 
           overlays = [ devshell.overlay ];
         };
-        in
-        pkgs.devshell.mkShell {
-          name = "scala";
-          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
-        };
+      in pkgs.devshell.mkShell {
+        name = "scala";
+        imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+      };
     });
 }
