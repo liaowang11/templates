4965e21c837ba5b361c10ac51553d50569a6d217
wliao <lwlw1988@gmail.com>
2021-12-28 17:57:52 +0800

    add zls

--- zig/flake.nix
+++ zig/flake.nix
@@ -3,12 +3,32 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
     zig.url = "github:arqv/zig-overlay";
+    zls = {
+      url = "https://github.com/zigtools/zls.git";
+      type = "git";
+      submodules = true;
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, utils, zig }:
+  outputs = { self, nixpkgs, utils, zig, zls }:
     utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-          zigLatest = zig.packages."${system}".master.latest;
+      let
+        pkgs = import nixpkgs { inherit system; };
+        zigLatest = zig.packages."${system}".master.latest;
+        zlsPackage = pkgs.stdenvNoCC.mkDerivation {
+          name = "zls";
+          version = "master";
+          src = zls;
+          nativeBuildInputs = [ zigLatest ];
+          dontConfigure = true;
+          dontInstall = true;
+          buildPhase = ''
+            mkdir -p $out
+            zig build install -Drelease-safe=true -Ddata_version=master --prefix $out
+          '';
+          XDG_CACHE_HOME = ".cache";
+        };
       in {
 
         # TDOO: default package and default app
@@ -16,10 +36,10 @@
 
         # defaultApp = utils.lib.mkApp { drv = self.defaultPackage."${system}"; };
 
-        devShell = with pkgs;
-          mkShell {
-            name = zigLatest.meta.name;
-            buildInputs = [ zigLatest ]; };
+        devShell = pkgs.mkShell {
+          name = zigLatest.meta.name;
+          buildInputs = [ zigLatest zlsPackage ];
+        };
       });
 
 }
