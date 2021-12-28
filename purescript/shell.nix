0d934a0c2d9bf26d0523991f62dae03b69620398
wliao <lwlw1988@gmail.com>
2021-12-29 00:30:43 +0800

    update purescript

--- purescript/shell.nix
+++ purescript/shell.nix
@@ -1,20 +1,7 @@
-{ pkgs ? import <nixpkgs> { } }:
-let
-  easy-ps = import (pkgs.fetchFromGitHub {
-    owner = "justinwoo";
-    repo = "easy-purescript-nix";
-    rev = "13ace3addf14dd9e93af9132e4799b7badfbe99e";
-    sha256 = "sha256-thMMJPmtk7tZKFjBg6HzcJcgPTNvixSThfJ5P0cIar8=";
-  }) { inherit pkgs; };
-in (pkgs.mkShell {
-  buildInputs = with easy-ps; [
-    purs-0_14_5
-    purs-tidy
-    psc-package
-    spago
-    purescript-language-server
-    pscid
-    pulp
-    pkgs.nodejs
-  ];
-}).overrideAttrs (oldAttrs: { name = "easy-purescript-nix"; })
+(import (
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
+    sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2"; }
+) {
+  src =  ./.;
+}).shellNix
