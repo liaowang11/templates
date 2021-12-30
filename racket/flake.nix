0e8893ba74671331dd3f81aa821bc4785bb7ac0e
wliao <lwlw1988@gmail.com>
2021-12-31 00:34:15 +0800

    add flake lock

--- racket/flake.nix
+++ racket/flake.nix
@@ -11,7 +11,7 @@
         in {
 
           devShell = pkgs.mkShell {
-            name = "racket";
+            name = "racket-${pkgs.racket.version}";
             buildInputs = with pkgs; [ racket ];
           };
 
